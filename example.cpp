8013fa30355ec1e36c8e3595979d213a2241c22a
Console <48036184+consolewritelin@users.noreply.github.com>
2019-02-27 03:53:26 +0300

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+
+	string name,surname,city;
+	int age;
+	double size;
+
+	cout << "Enter Your Name = ";
+	cin >> name;
+	cout << "Enter Your Surname = ";
+	cin >> surname;
+	cout << "Wher are you from = ";
+	cin >> city;
+	cout << "How Old Are You =";
+	cin >> age;
+	cout << "Enter Your Height =";
+	cin >> size;
+
+	cout << "Name : " << name << endl;
+	cout << "Surname : " << surname << endl;
+	cout << "city : " << city << endl;
+	cout << "Age : " << age << endl;
+	cout << "Size : " << size << endl;
+
+
+
+	return 0;
+}
