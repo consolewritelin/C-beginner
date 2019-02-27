19a256e72ecbb6b4acb3a80bd1072a20e2d8bb85
consolewritelin <48036184+consolewritelin@users.noreply.github.com>
2019-02-27 03:26:31 +0300

    Add files via upload
    Description of textual expressions

---
+++ textual expressions .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+	string name,surname;
+
+	cout << "Enter Your Name :" << endl;
+	cin >> name;
+	cout << "Enter Your Surname :" << endl;
+	cin >> surname;
+	cout << "Dear " << name << " " << surname << "\n\nWelcome to our program.";
+
+	return 0;
+}
